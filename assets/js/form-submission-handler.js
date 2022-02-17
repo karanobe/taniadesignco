f5c2679324f9b0e3e6a0f544f4d0ab916f3aaf5b
karanobe <koberoi1017@gmail.com>
2022-02-17 13:40:11 -0800

    add website template

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -1,105 +1,27 @@
-(function() {
-  // get all data in form and return object
-  function getFormData(form) {
-    var elements = form.elements;
-    var honeypot;
+$('.contact').on('submit',function(e){
+    //optional validation code here
 
-    var fields = Object.keys(elements).filter(function(k) {
-      if (elements[k].name === "honeypot") {
-        honeypot = elements[k].value;
-        return false;
-      }
-      return true;
-    }).map(function(k) {
-      if(elements[k].name !== undefined) {
-        return elements[k].name;
-      // special case for Edge's html collection
-      }else if(elements[k].length > 0){
-        return elements[k].item(0).name;
-      }
-    }).filter(function(item, pos, self) {
-      return self.indexOf(item) == pos && item;
-    });
-
-    var formData = {};
-    fields.forEach(function(name){
-      var element = elements[name];
-      
-      // singular form elements just have one value
-      formData[name] = element.value;
-
-      // when our element has multiple items, get their values
-      if (element.length) {
-        var data = [];
-        for (var i = 0; i < element.length; i++) {
-          var item = element.item(i);
-          if (item.checked || item.selected) {
-            data.push(item.value);
-          }
-        }
-        formData[name] = data.join(', ');
-      }
-    });
-
-    // add form-specific values into the data
-    formData.formDataNameOrder = JSON.stringify(fields);
-    formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
-    formData.formGoogleSendEmail
-      = form.dataset.email || ""; // no email by default
-
-    return {data: formData, honeypot: honeypot};
-  }
-
-  function handleFormSubmit(event) {  // handles form submit without any jquery
-    event.preventDefault();           // we are submitting via xhr below
-    var form = event.target;
-    var formData = getFormData(form);
-    var data = formData.data;
-
-    // If a honeypot field is filled, assume it was done so by a spam bot.
-    if (formData.honeypot) {
-      return false;
-    }
-
-    disableAllButtons(form);
-    var url = form.action;
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
-    // xhr.withCredentials = true;
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          form.reset();
-          var formElements = $('.contact-form__inner');
-          if (formElements) {
-            formElements.hide();
-          }        
-          var thankYouMessage = $('.thankyou_message');
-          if (thankYouMessage) {
-            thankYouMessage.fadeIn();
-          }
-        }
-    };
-    // url encode form data for sending as post data
-    var encoded = Object.keys(data).map(function(k) {
-        return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
-    }).join('&');
-    xhr.send(encoded);
-  }
+    e.preventDefault();
   
-  function loaded() {
-    // bind to the submit event of our form
-    var forms = document.querySelectorAll("form.gform");
-    for (var i = 0; i < forms.length; i++) {
-      forms[i].addEventListener("submit", handleFormSubmit, false);
-    }
-  };
-  document.addEventListener("DOMContentLoaded", loaded, false);
-
-  function disableAllButtons(form) {
-    var buttons = form.querySelectorAll("button");
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true;
-    }
-  }
-})();
+    $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbx9P6Q8P9Ek_HLSSXt0MoPuki8EgKdgKrCU810r6vo8xQ4tB_iX2WCDOlKbmiyEMg/exec",
+        method: "POST",
+        dataType: "json",
+        data: $(".contact").serialize(),
+        success: function(response) {
+            
+            if(response.result == "success") {
+                $('.contact')[0].reset();
+                alert('Thank you for contacting us.');
+                return true;
+            }
+            else {
+                alert("Something went wrong. Please try again.")
+            }
+        },
+        error: function() {
+            
+            alert("Something went wrong. Please try again.")
+        }
+    })
+});
